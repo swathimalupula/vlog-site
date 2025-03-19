bdffc47a566739a2498a6d22e367fa11f02a8c49
Malupula Ashok <malupulaashok@Malupulas-MacBook-Pro.local>
2025-03-19 14:53:38 +0530

    Update views and URLs

--- blog1/views.py
+++ blog1/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from .models import Vlog, Photo
+from .models import Vlog, Photo  # Correct import path
 
 def home(request):
     vlogs = Vlog.objects.all().order_by('-created_at')
     photos = Photo.objects.all().order_by('-uploaded_at')
-    return render(request, 'blog1/home.html', {'vlogs': vlogs, 'photos': photos}) 
-
-# Create your views here.
+    return render(request, 'blog1/home.html', {'vlogs': vlogs, 'photos': photos})
