89f3b2928a0d43b62e678e9060f6bbd208bd616f
Malupula Ashok <malupulaashok@Malupulas-MacBook-Pro.local>
2025-03-19 13:40:14 +0530

    Set DEBUG to False in settings.py

--- vlogsite1/settings.py
+++ vlogsite1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qr=%ajn+fjzy_=3t2hwrve4bbrnyp880l6xnmzvxy%6hm7xfwu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'vlog-site-7.onrender.com',
