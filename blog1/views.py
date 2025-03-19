bd435c33d3f332a48d2c0cc73e21ee8c72d24d15
Malupula Ashok <malupulaashok@Malupulas-MacBook-Pro.local>
2025-03-19 14:03:48 +0530

    Update views and URLs for [feature or bug fix]

--- blog1/views.py
+++ blog1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Vlog, Photo
+
+def home(request):
+    vlogs = Vlog.objects.all().order_by('-created_at')
+    photos = Photo.objects.all().order_by('-uploaded_at')
+    return render(request, 'blog1/home.html', {'vlogs': vlogs, 'photos': photos}) 
 
 # Create your views here.
